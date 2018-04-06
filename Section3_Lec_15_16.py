e65f6e5abfc1533aed0480b790c624581e5edf56
dldeshpande <dldeshpande@gmail.com>
2018-04-06 18:10:04 +0530

    Section 3_Lect1_13_14

---
+++ Section3_Lec_15_16.py
@@ -0,0 +1,14 @@
+Greeting = "Hello World"
+print(Greeting)
+Greeting = Greeting.split(" ")[0]
+print(Greeting)
+print(Greeting + " John ")
+print(Greeting + " Guy's ")
+
+Greeting = Greeting + " Burg "
+print(Greeting)
+
+number = 1
+secondnumber = 2
+thirdnumber = 8
+print(number*secondnumber + secondnumber * thirdnumber)
