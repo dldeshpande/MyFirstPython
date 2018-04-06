a1c1512e7c44ea60bc0d66d03b5c254cd0102a63
dldeshpande <dldeshpande@gmail.com>
2018-04-06 18:26:11 +0530

    Section 3_Lect1_13_14

--- Section3_Lec_15_16.py
+++ Section3_Lec_15_16.py
@@ -12,3 +12,14 @@ number = 1
 secondnumber = 2
 thirdnumber = 8
 print(number*secondnumber + secondnumber * thirdnumber)
+
+str("5")
+str("2.12")
+bool("True")
+float(2.15)
+
+
+len([1,2,8,9]
+len(["one","two","three"])
+sorted([1,15,17,90,74,55])
+sorted(["A","B","Z","W","M","0","5","2.5","a"])
