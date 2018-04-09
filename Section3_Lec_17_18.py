20aea345f7fb41a4b76eeae93a9f0158cbd0ad94
dldeshpande <dldeshpande@gmail.com>
2018-04-09 15:53:35 +0530

    Section 3_Lect1_17_18

---
+++ Section3_Lec_17_18.py
@@ -0,0 +1,19 @@
+
+
+def my_first_function():
+    print("Function in python")
+    print("how to call function" + " in python")
+
+
+my_first_function()
+
+# passing argument in function
+
+
+def my_first_function(fun1, fun2):
+    print(fun1)
+    print(fun2)
+
+
+my_first_function("first argument", "second argument")
+my_first_function("third argument", "fourth argument")
