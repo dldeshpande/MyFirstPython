fd83755b41e6b141e4061002af0b3f05ac6eeed3
dldeshpande <dldeshpande@gmail.com>
2018-04-09 22:57:32 +0530

    Section 3_Lect1_20_21

--- Section3_Lec_20_21.py
+++ Section3_Lec_20_21.py
@@ -14,3 +14,14 @@ def print_king(*solder):
         print("This is :", person)
 
 print_king("joe","mark","henry","raw")
+
+# return values from function
+
+def print_add(num1,num2):
+    return  num1 + num2
+
+math1=print_add(10,20)
+math2=print_add(54,50)
+print("The addition of 1st number is : ", math1, "The addition of 2nd number is : ", math2)
+
+
